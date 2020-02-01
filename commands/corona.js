62e1764327ddaeecd87638c382b45996b80f0d1b
Yuu <okumura6883@gmail.com>
2020-01-31 23:06:43 -0800

    Added option to fetch from either QQ or JH for corona command

--- commands/corona.js
+++ commands/corona.js
@@ -4,24 +4,40 @@ const Corona = require("../src/modules/Corona");
 /** @type {BaseCommand} */
 module.exports = {
     verify: async function(message) {
-        return /^corona$/.test(message.content);
+        return /^corona\b/.test(message.content);
     },
     run: async function(message) {
-        try {
-            let result = await Corona.fromQQ();
-            let embed = new RichEmbed()
-                .setTitle("China Corona Virus Report " + result.chinaTotal.date)
-                .setURL("https://news.qq.com/zt2020/page/feiyan.htm")
-                .setThumbnail("https://riotimes-11af1.kxcdn.com/wp-content/uploads/2020/01/virus.jpg")
-                .addField("Infected",  `**${result.chinaTotal.confirm}**`)
-                .addField("Suspect",   `**${result.chinaTotal.suspect}**`)
-                .addField("Recovered", `**${result.chinaTotal.heal}**`)
-                .addField("Dead",      `**${result.chinaTotal.dead}**`)
-                .setFooter(`Last Update: ${result.lastUpdateTime} (GMT+8)`);
-            message.channel.send(embed);
-        } catch (e) {
-            this.logger.onError(e);
-            message.channel.send("Failed to fetch from source");
-        }
+        if (message.content.toLowerCase().includes("qq")) {
+            try {
+                let result = await Corona.fromQQ();
+                let embed = new RichEmbed()
+                    .setTitle("Corona Virus Report from QQ " + result.chinaTotal.date)
+                    .setURL("https://news.qq.com/zt2020/page/feiyan.htm")
+                    .setThumbnail("https://riotimes-11af1.kxcdn.com/wp-content/uploads/2020/01/virus.jpg")
+                    .addField("Infected",  `**${result.chinaTotal.confirm}**`)
+                    .addField("Suspect",   `**${result.chinaTotal.suspect}**`)
+                    .addField("Recovered", `**${result.chinaTotal.heal}**`)
+                    .addField("Dead",      `**${result.chinaTotal.dead}**`)
+                    .setFooter(`Last Update: ${result.lastUpdateTime} (GMT+8)`);
+                message.channel.send(embed);
+            } catch (e) {
+                this.logger.onError(e);
+                message.channel.send("Failed to fetch from source");
+            }
+        } else {
+            try {
+                let result = await Corona.fromJH();let embed = new RichEmbed()
+                    .setTitle("Corona Virus Report from Johns Hopkins CSSE")
+                    .setURL("https://gisanddata.maps.arcgis.com/apps/opsdashboard/index.html#/85320e2ea5424dfaaa75ae62e5c06e61")
+                    .setThumbnail("https://riotimes-11af1.kxcdn.com/wp-content/uploads/2020/01/virus.jpg")
+                    .addField("Total Confirmed", `**${result.total}**`)
+                    .addField("Total Recovered", `**${result.recover}**`)
+                    .addField("Total Death",     `**${result.death}**`)
+                message.channel.send(embed);
+            } catch (e) {
+                this.logger.onError(e);
+                message.channel.send("Failed to fetch from source");
+            }
+        }        
     }
 }
--- src/modules/Corona.js
+++ src/modules/Corona.js
@@ -10,3 +10,16 @@ module.exports.fromQQ = async () => {
     let obj = JSON.parse(text);
     return JSON.parse(obj.data);
 }
+
+module.exports.fromJH = async () => {
+    let recoverRes = await fetch(`https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/ncov_cases/FeatureServer/1/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&outStatistics=%5B%7B%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22Recovered%22%2C%22outStatisticFieldName%22%3A%22value%22%7D%5D&outSR=102100&cacheHint=true`)
+    let res = await recoverRes.json();
+    let recover = ~~res.features[0].attributes.value;
+    let deathRes = await fetch(`https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/ncov_cases/FeatureServer/1/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&outStatistics=%5B%7B%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22Deaths%22%2C%22outStatisticFieldName%22%3A%22value%22%7D%5D&outSR=102100&cacheHint=true`);
+    res = await deathRes.json();
+    let death = ~~res.features[0].attributes.value;
+    let totalRes = await fetch(`https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/ncov_cases/FeatureServer/1/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&outStatistics=%5B%7B%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22Confirmed%22%2C%22outStatisticFieldName%22%3A%22value%22%7D%5D&outSR=102100&cacheHint=true`);
+    res = await totalRes.json();
+    let total = ~~res.features[0].attributes.value;
+    return { recover, death, total };
+}
