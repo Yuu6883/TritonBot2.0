73937fdd5ab8a912bb62a9ff05e081f1359d75e8
Yuu <okumura6883@gmail.com>
2020-02-06 18:14:19 -0800

    :gary: tested this module and approved it

--- test/GOLTest.js
+++ test/GOLTest.js
@@ -1,5 +1,12 @@
 const GOL = require("../src/modules/GameOfLife");
+const { loadImage } = require("canvas");
 const fs = require("fs");
+const path = require("path");
 GOL.DEBUG = true;
-let gol = GOL.fromText(fs.readFileSync("test.txt", "utf-8"));
-gol.renderVideo().then(console.log);
+
+// let gol = GOL.fromText(fs.readFileSync("test.txt", "utf-8"));
+// gol.renderVideo().then(console.log); 
+
+loadImage(path.resolve(__dirname, "gary.jpg")).then(img => {
+    GOL.fromImage(img, true).renderVideo().then(console.log);
+});
